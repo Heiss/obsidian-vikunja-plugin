1da2c98638f43f740d7a5b8af6665fb0c345d57a
Peter Heiss <peter.heiss@uni-muenster.de>
2024-06-18 18:32:28 +0200

    init commit

---
+++ vikunja_sdk/apis/index.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ApiApi';
+export * from './AssigneesApi';
+export * from './AuthApi';
+export * from './FilterApi';
+export * from './LabelsApi';
+export * from './MigrationApi';
+export * from './ProjectApi';
+export * from './ServiceApi';
+export * from './SharingApi';
+export * from './SubscriptionsApi';
+export * from './TaskApi';
+export * from './TeamApi';
+export * from './TestingApi';
+export * from './UserApi';
+export * from './WebhooksApi';
